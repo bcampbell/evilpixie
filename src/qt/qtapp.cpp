98d2f6256e1ed049681e44d53fcca420114f1313
Ben Campbell <ben@scumways.com>
2011-04-29 22:15:43 +0100

    load files passed in via commandline

--- src/qt/qtapp.cpp
+++ src/qt/qtapp.cpp
@@ -1,9 +1,11 @@
 #include "qtapp.h"
 
 #include "../editor.h"
+#include "../wobbly.h"
 #include "editorwindow.h"
 #include <cstdio>
 #include <QApplication>
+#include <QMessageBox>
 
 
 
@@ -20,8 +22,32 @@ int QTApp::Run( int argc, char* argv[] )
 {
     QApplication app(argc, argv);
 
-    EditorWindow fenster(new Project());
-    fenster.show();
+    QStringList args = app.arguments();
+
+    int i;
+    int cnt = 0;
+    for(i=1;i<args.size();++i)
+    {
+        try
+        {
+            std::string filename( args.at(i).toStdString() );
+            Project* p = new Project(filename.c_str());
+            EditorWindow* fenster = new EditorWindow(p);
+            fenster->show();
+            ++cnt;
+        }
+        catch( Wobbly const& e )
+        {
+            //GUIShowError( e.what() );
+            QMessageBox::warning( 0, "Error", e.what() );
+        }
+    }
+    if(!cnt)
+    {
+        EditorWindow* fenster = new EditorWindow(new Project());
+        fenster->show();
+    }
+
     return app.exec();
 }
 
