bc1a39fd8e271ada080c19251b591ab3fb7bffbe
Ben Campbell <ben@scumways.com>
2015-08-04 10:18:41 +1200

    update blit zoom fns to handle RGBA8

--- src/draw.cpp
+++ src/draw.cpp
@@ -424,7 +424,60 @@ void BlitMatteRGBA8(
 
 
 
-// XYZZY
+static void scan_zoom_keyed_I8_RGBX8(I8 const* src, Palette const& pal, RGBX8* dest, int w, I8 transparent, int xzoom)
+{
+    int n=0;
+    int x;
+    for( x=0; x<w; ++x )
+    {
+        if(*src != transparent)
+        {
+            RGBX8 c = pal.GetColour(*src);
+            *dest = c;
+        }
+        ++dest;
+        if( ++n >= xzoom )
+        {
+            ++src;
+            n=0;
+        }
+    }
+}
+
+static void scan_zoom_keyed_RGBX8_RGBX8(RGBX8 const* src, RGBX8* dest, int w, RGBX8 transparent, int xzoom)
+{
+    int n=0;
+    int x;
+    for( x=0; x<w; ++x )
+    {
+        RGBX8 c=*src;
+        if( c != transparent)
+            *dest = c;
+        ++dest;
+        if( ++n >= xzoom )
+        {
+            ++src;
+            n=0;
+        }
+    }
+}
+
+static void scan_zoom_alpha_RGBA8_RGBX8(RGBA8 const* src, RGBX8* dest, int w, int xzoom)
+{
+    int n=0;
+    int x;
+    for( x=0; x<w; ++x )
+    {
+        RGBX8 out = Blend(*src, *dest);
+        *dest++ = out;
+        if( ++n >= xzoom )  // on to next src pixel?
+        {
+            ++src;
+            n=0;
+        }
+    }
+}
+
 void BlitZoomTransparent(
     Img const& srcimg, Box const& srcbox,
     Img& destimg, Box& destbox,
@@ -445,52 +498,32 @@ void BlitZoomTransparent(
     int y;
     for( y=0; y<destclipped.H(); ++y )
     {
-        int x;
         RGBX8* dest = destimg.Ptr_RGBX8( destclipped.XMin() + 0, destclipped.YMin() + y );
         switch(srcimg.Fmt())
         {
         case FMT_I8:
-            {
-                I8 const* src = srcimg.PtrConst_I8( srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom );
-                int n=0;
-                for( x=0; x<destclipped.W(); ++x )
-                {
-                    if( *src != transparentcolour.idx())
-                    {
-                        RGBX8 c = palette.GetColour(*src);
-                        *dest = c;
-                    }
-                    ++dest;
-                    if( ++n >= xzoom )
-                    {
-                        ++src;
-                        n=0;
-                    }
-                }
-            }
+            scan_zoom_keyed_I8_RGBX8(
+                srcimg.PtrConst_I8( srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom ),
+                palette,
+                dest,
+                destclipped.W(),
+                transparentcolour.idx(),
+                xzoom );
             break;
         case FMT_RGBX8:
-            {
-                RGBX8 const* src = srcimg.PtrConst_RGBX8( srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom );
-                int n=0;
-                for( x=0; x<destclipped.W(); ++x )
-                {
-                    RGBX8 c=*src;
-                    if( c != transparentcolour.rgb())
-                        *dest = c;
-                    ++dest;
-                    if( ++n >= xzoom )
-                    {
-                        ++src;
-                        n=0;
-                    }
-                }
-            }
+            scan_zoom_keyed_RGBX8_RGBX8(
+                srcimg.PtrConst_RGBX8( srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom ),
+                    dest,
+                    destclipped.W(),
+                    transparentcolour.rgb(),
+                    xzoom );
             break;
         default:
-    // XYZZY - TODO!
-  return;
-            assert(false);
+            scan_zoom_alpha_RGBA8_RGBX8(
+                srcimg.PtrConst_RGBA8(srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom),
+                    dest,
+                    destclipped.W(),
+                    xzoom );
             break;
         }
     }
@@ -498,7 +531,27 @@ void BlitZoomTransparent(
 
 
 
-// XYZZY
+static void scan_matte_zoom_alpha_RGBA8_RGBX8(RGBA8 const* src, RGBX8* dest, int w, RGBA8 matte, int xzoom)
+{
+    int n=0;
+    int x;
+    for( x=0; x<w; ++x )
+    {
+        RGBA8 in = *src;
+        in.r = matte.r;
+        in.g = matte.g;
+        in.b = matte.b;
+        RGBX8 out = Blend(in, *dest);
+        *dest++ = out;
+        if( ++n >= xzoom )  // on to next src pixel?
+        {
+            ++src;
+            n=0;
+        }
+    }
+}
+
+
 void BlitZoomMatte(
     Img const& srcimg, Box const& srcbox,
     Img& destimg, Box& destbox,
@@ -525,6 +578,7 @@ void BlitZoomMatte(
         {
         case FMT_I8:
             {
+                // TODO: move into scan fn
                 I8 const* src = srcimg.PtrConst_I8( srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom );
                 int n=0;
                 for( x=0; x<destclipped.W(); ++x )
@@ -542,6 +596,7 @@ void BlitZoomMatte(
             break;
         case FMT_RGBX8:
             {
+                // TODO: move into scan fn
                 RGBX8 const* src = srcimg.PtrConst_RGBX8( srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom );
                 int n=0;
                 for( x=0; x<destclipped.W(); ++x )
@@ -558,11 +613,13 @@ void BlitZoomMatte(
             }
             break;
         default:
-    // XYZZY - TODO!
-return;
-            assert(false);
+            scan_matte_zoom_alpha_RGBA8_RGBX8(
+                srcimg.PtrConst_RGBA8(srcclipped.XMin()+0, srcclipped.YMin()+y/yzoom),
+                dest,
+                destclipped.W(),
+                mattecolour.rgb(),
+                xzoom );
             break;
-
         }
     }
 }
