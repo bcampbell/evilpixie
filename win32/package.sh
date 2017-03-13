bb5cdf59e3ae53090d54e2e284675fe55b310bef
Ben Campbell <ben@scumways.com>
2017-03-13 23:11:01 +1300

    win32: First working .msi installer (with rough edges!)

---
+++ win32/package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+# generate a .msi installer from a build (in ./evilpixie)
+
+# data.wxs was originally auto-generated, then some dir structure hand-fiddled.
+# original generation was:
+#   heat dir evilpixie -gg -srd -sreg -suid -dr INSTALLDIR -cg grp_data -out data.wxs
+
+
+candle -arch x64 main.wxs data.wxs
+light main.wixobj data.wixobj -b evilpixie -o evilpixie-0.2.msi
+
